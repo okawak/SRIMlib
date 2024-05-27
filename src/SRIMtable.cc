508e6c5b97a3ae752a2023935eeb26eb45b88107
okawak <kodak2022@outlook.jp>
2024-05-27 18:48:15 +0900

    del: unused binary

--- src/SRIMtable.cc
+++ src/SRIMtable.cc
@@ -1,139 +1,132 @@
 /*
-   @File name     : SRIMtable.cc
-   @description   : useful function from SRIM output textfiles
-   @Author        : Kodai Okawa<okawa@cns.s.u-tokyo.ac.jp>
-   @Created date  : 2023-05-26 17:52:55
-   @Last modified : 2023-07-27 16:16:55
-*/
+ * @File name     : SRIMtable.cc
+ * @description   : useful function from SRIM output textfiles
+ * @Author        : Kodai Okawa<okawa@cns.s.u-tokyo.ac.jp>
+ * @Created date  : 2023-05-26 17:52:55
+ * @Last modified : 2023-07-27 16:16:55
+ */
 
-#include <iostream>
 #include <TFile.h>
+#include <iostream>
 
 #include <SRIMtable.h>
 
-SRIMtable::SRIMtable(TString name, Double_t e_min, Double_t e_max, Double_t t_max)
-{
-   if (!getenv("SRIMLIB_HOME")) {
-      std::cerr << "ERROR: load thisSRIMlib.sh" << std::endl;
-      exit(1);
-   }
-   TString workdir = getenv("SRIMLIB_HOME");
-   TString rootpath = workdir + "/db/SRIMdata.root";
+SRIMtable::SRIMtable(TString name, Double_t e_min, Double_t e_max, Double_t t_max) {
+    if (!getenv("SRIMLIB_HOME")) {
+        std::cerr << "ERROR: load thisSRIMlib.sh" << std::endl;
+        exit(1);
+    }
+    TString workdir = getenv("SRIMLIB_HOME");
+    TString rootpath = workdir + "/db/SRIMdata.root";
 
-   min = e_min;
-   max = e_max;
-   thick_max = t_max;
+    min = e_min;
+    max = e_max;
+    thick_max = t_max;
 
-   TFile *file = new TFile(rootpath, "read");
-   if (!file) {
-      std::cerr << "ERROR : firstly update command needed" << std::endl;
-      exit(1);
-   }
+    TFile *file = new TFile(rootpath, "read");
+    if (!file) {
+        std::cerr << "ERROR : firstly update command needed" << std::endl;
+        exit(1);
+    }
 
-   // eloss_e = (TSpline3 *)file->Get(name + "_eloss_e");
-   // eloss_n = (TSpline3 *)file->Get(name + "_eloss_n");
-   eloss_tot = (TSpline3 *)file->Get(name + "_eloss_tot");
-   range = (TSpline3 *)file->Get(name + "_range");
-   inv_range = (TSpline3 *)file->Get(name + "_inv_range");
-   strg_y = (TSpline3 *)file->Get(name + "_strg_y");
-   strg_x = (TSpline3 *)file->Get(name + "_strg_x");
+    // eloss_e = (TSpline3 *)file->Get(name + "_eloss_e");
+    // eloss_n = (TSpline3 *)file->Get(name + "_eloss_n");
+    eloss_tot = (TSpline3 *)file->Get(name + "_eloss_tot");
+    range = (TSpline3 *)file->Get(name + "_range");
+    inv_range = (TSpline3 *)file->Get(name + "_inv_range");
+    strg_y = (TSpline3 *)file->Get(name + "_strg_y");
+    strg_x = (TSpline3 *)file->Get(name + "_strg_x");
 
-   file->Close();
+    file->Close();
 }
 
 SRIMtable::~SRIMtable() {}
 
-Double_t SRIMtable::GetEloss(Double_t energy)
-{
-   Int_t flag = EnergyCheck(energy);
-   if (flag == 0) {
-      return 0.0;
-   }
-   Double_t result = eloss_tot->Eval(energy);
-   return result;
+Double_t SRIMtable::GetEloss(Double_t energy) {
+    Int_t flag = EnergyCheck(energy);
+    if (flag == 0) {
+        return 0.0;
+    }
+    Double_t result = eloss_tot->Eval(energy);
+    return result;
 }
 
-Double_t SRIMtable::GetNewE(Double_t energy, Double_t thickness)
-{
-   Int_t flag = EnergyCheck(energy);
-   if (flag == 0) {
-      return 0.0;
-   }
-   Bool_t isNormal = ThicknessCheck(thickness);
-   if (!isNormal)
-      return 0.0;
+Double_t SRIMtable::GetNewE(Double_t energy, Double_t thickness) {
+    Int_t flag = EnergyCheck(energy);
+    if (flag == 0) {
+        return 0.0;
+    }
+    Bool_t isNormal = ThicknessCheck(thickness);
+    if (!isNormal)
+        return 0.0;
 
-   Double_t result = energy;
-   Double_t ini_range = range->Eval(energy);
-   if (ini_range - thickness < range->Eval(range->GetXmin()))
-      result = 0.0;
-   else
-      result = inv_range->Eval(ini_range - thickness);
+    Double_t result = energy;
+    Double_t ini_range = range->Eval(energy);
+    if (ini_range - thickness < range->Eval(range->GetXmin()))
+        result = 0.0;
+    else
+        result = inv_range->Eval(ini_range - thickness);
 
-   return result;
+    return result;
 }
 
-Double_t SRIMtable::GetOldE(Double_t energy, Double_t thickness)
-{
-   Int_t flag = EnergyCheck(energy);
-   if (flag == 0) {
-      std::cerr << "out of energy range" << std::endl;
-      std::exit(1);
-   }
-   Bool_t isNormal = ThicknessCheck(thickness);
-   if (!isNormal) {
-      std::cerr << "out of thickenss range" << std::endl;
-      std::exit(1);
-   }
+Double_t SRIMtable::GetOldE(Double_t energy, Double_t thickness) {
+    Int_t flag = EnergyCheck(energy);
+    if (flag == 0) {
+        std::cerr << "out of energy range" << std::endl;
+        std::exit(1);
+    }
+    Bool_t isNormal = ThicknessCheck(thickness);
+    if (!isNormal) {
+        std::cerr << "out of thickenss range" << std::endl;
+        std::exit(1);
+    }
 
-   Double_t result = energy;
-   Double_t ini_range = range->Eval(energy);
-   if (ini_range + thickness > range->Eval(range->GetXmax())) {
-      std::cerr << "Warning: out of range" << std::endl;
-      result = 0.0;
-   } else
-      result = inv_range->Eval(ini_range + thickness);
+    Double_t result = energy;
+    Double_t ini_range = range->Eval(energy);
+    if (ini_range + thickness > range->Eval(range->GetXmax())) {
+        std::cerr << "Warning: out of range" << std::endl;
+        result = 0.0;
+    } else
+        result = inv_range->Eval(ini_range + thickness);
 
-   return result;
+    return result;
 }
 
-Double_t SRIMtable::GetRange(Double_t energy)
-{
-   Int_t flag = EnergyCheck(energy);
-   if (flag == 0) {
-      return 0.0;
-   }
-   Double_t result = range->Eval(energy);
-   return result;
+Double_t SRIMtable::GetRange(Double_t energy) {
+    Int_t flag = EnergyCheck(energy);
+    if (flag == 0) {
+        return 0.0;
+    }
+    Double_t result = range->Eval(energy);
+    return result;
 }
 
-Double_t SRIMtable::GetStrg(Double_t energy)
-{
-   Int_t flag = EnergyCheck(energy);
-   if (flag == 0) {
-      return 0.0;
-   }
-   Double_t result = strg_y->Eval(energy);
-   return result;
+Double_t SRIMtable::GetStrg(Double_t energy) {
+    Int_t flag = EnergyCheck(energy);
+    if (flag == 0) {
+        return 0.0;
+    }
+    Double_t result = strg_y->Eval(energy);
+    return result;
 }
 
 Int_t SRIMtable::EnergyCheck(Double_t energy) // 0: zero 1: normal
 {
-   if (max < energy) {
-      std::cerr << "ERROR: out of range (input energy < " << max << " needed)" << std::endl;
-      std::exit(1);
-   }
+    if (max < energy) {
+        std::cerr << "ERROR: out of range (input energy < " << max << " needed)" << std::endl;
+        std::exit(1);
+    }
 
-   if (energy < min) {
-      return 0;
-   }
-   return 1;
+    if (energy < min) {
+        return 0;
+    }
+    return 1;
 }
 
-Bool_t SRIMtable::ThicknessCheck(Double_t thickness)
-{
-   if (thickness > thick_max) {
-      return false;
-   }
-   return true;
+Bool_t SRIMtable::ThicknessCheck(Double_t thickness) {
+    if (thickness > thick_max) {
+        return false;
+    }
+    return true;
 }
--- src/allinfo.cc
+++ src/allinfo.cc
@@ -1,80 +1,79 @@
 /*
-   @File name     : allinfo.cc
-   @description   :
-   @Author        : Kodai Okawa<okawa@cns.s.u-tokyo.ac.jp>
-   @Created date  : 2023-05-26 18:16:08
-   @Last modified : 2023-06-07 17:39:27
-*/
+ * @File name     : allinfo.cc
+ * @description   :
+ * @Author        : Kodai Okawa<okawa@cns.s.u-tokyo.ac.jp>
+ * @Created date  : 2023-05-26 18:16:08
+ * @Last modified : 2023-06-07 17:39:27
+ */
 
-#include <iostream>
 #include <TROOT.h>
+#include <iostream>
 
 #include <SRIMData.h>
 #include <SRIMtable.h>
 
-Int_t main()
-{
-   TString dummy = "dummy";
-   Double_t dtmp = 0.0;
-   std::cin.exceptions(std::ios::failbit);
+Int_t main() {
+    TString dummy = "dummy";
+    Double_t dtmp = 0.0;
+    std::cin.exceptions(std::ios::failbit);
 
-   SRIMData *data = new SRIMData();
-   data->PrintBeams();
-   std::cout << "select the beam particle : ";
-   std::cin >> dummy;
-   TString beamname = dummy;
-   std::cout << std::endl;
+    SRIMData *data = new SRIMData();
+    data->PrintBeams();
+    std::cout << "select the beam particle : ";
+    std::cin >> dummy;
+    TString beamname = dummy;
+    std::cout << std::endl;
 
-   data->PrintTargets(beamname);
-   std::cout << "select the target material : ";
-   std::cin >> dummy;
-   TString targetname = dummy;
-   std::cout << std::endl;
+    data->PrintTargets(beamname);
+    std::cout << "select the target material : ";
+    std::cin >> dummy;
+    TString targetname = dummy;
+    std::cout << std::endl;
 
-   SRIMtable *table = data->GetTable(beamname, targetname);
-   std::cout << "input the initial energy (MeV) : ";
-   while (true) {
-      try {
-         std::cin >> dtmp;
-      } catch (...) {
-         std::cerr << "ERROR: input the double number : ";
+    SRIMtable *table = data->GetTable(beamname, targetname);
+    std::cout << "input the initial energy (MeV) : ";
+    while (true) {
+        try {
+            std::cin >> dtmp;
+        } catch (...) {
+            std::cerr << "ERROR: input the double number : ";
 
-         std::cin.clear();
-         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-         continue;
-      }
-      break;
-   }
-   Double_t ini_energy = dtmp;
-   std::cout << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        break;
+    }
+    Double_t ini_energy = dtmp;
+    std::cout << std::endl;
 
-   std::cout << "input the material thickness (mm) : ";
-   while (true) {
-      try {
-         std::cin >> dtmp;
-      } catch (...) {
-         std::cout << "ERROR: input the double number : ";
+    std::cout << "input the material thickness (mm) : ";
+    while (true) {
+        try {
+            std::cin >> dtmp;
+        } catch (...) {
+            std::cout << "ERROR: input the double number : ";
 
-         std::cin.clear();
-         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-         continue;
-      }
-      break;
-   }
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        break;
+    }
 
-   Double_t thickness = dtmp;
+    Double_t thickness = dtmp;
 
-   Double_t eloss = table->GetEloss(ini_energy);
-   Double_t newe = table->GetNewE(ini_energy, dtmp);
-   Double_t olde = table->GetOldE(ini_energy, dtmp);
-   Double_t range = table->GetRange(ini_energy);
-   Double_t strg = table->GetStrg(ini_energy);
+    Double_t eloss = table->GetEloss(ini_energy);
+    Double_t newe = table->GetNewE(ini_energy, dtmp);
+    Double_t olde = table->GetOldE(ini_energy, dtmp);
+    Double_t range = table->GetRange(ini_energy);
+    Double_t strg = table->GetStrg(ini_energy);
 
-   std::cout << "energy loss : " << eloss << " keV/um (MeV/mm)" << std::endl;
-   std::cout << "newe        : " << newe << " MeV" << std::endl;
-   std::cout << "olde        : " << olde << " MeV" << std::endl;
-   std::cout << "Range       : " << range << " mm" << std::endl;
-   std::cout << "straggling  : " << strg << " mm" << std::endl;
+    std::cout << "energy loss : " << eloss << " keV/um (MeV/mm)" << std::endl;
+    std::cout << "newe        : " << newe << " MeV" << std::endl;
+    std::cout << "olde        : " << olde << " MeV" << std::endl;
+    std::cout << "Range       : " << range << " mm" << std::endl;
+    std::cout << "straggling  : " << strg << " mm" << std::endl;
 
-   return 0;
+    return 0;
 }
